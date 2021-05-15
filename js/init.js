2e07b2916776daa0c6a147b3203ddd90c373ab45
Shivaprasad <shiva02prasad@gmail.com>
2021-05-15 22:01:13 +0530

    Extand animation file

---
+++ js/init.js
@@ -0,0 +1,24 @@
+// -----------------------------------------------------
+// -----------------    PROGRESS BAR    ----------------
+// -----------------------------------------------------
+
+function tdProgress(container){
+
+	"use strict";
+
+	container.find('.marox_tm_progress').each(function(i) {
+		var progress 		= jQuery(this);
+		var pValue 			= parseInt(progress.data('value'), 10);
+		var pColor			= progress.data('color');
+		var pBarWrap 		= progress.find('.marox_tm_bar_wrap');
+		var pBar 			= progress.find('.marox_tm_bar');
+		pBar.css({width:pValue+'%', backgroundColor:pColor});
+		setTimeout(function(){pBarWrap.addClass('open');},(i*500));
+	});
+}
+jQuery('.marox_tm_progress_wrap').each(function() {
+	"use strict";
+	var pWrap 			= jQuery(this);
+	pWrap.waypoint({handler: function(){tdProgress(pWrap);},offset:'90%'});	
+});
+
